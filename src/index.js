0337c58e62ef858264ada259112bb2cbf21091fd
claracampos <camposclr@gmail.com>
2020-02-19 11:56:09 -0400

    added test reading routes/fetching

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+require("./database.js");
+const User = require("./models/user");
+const Entry = require("./models/entry");
+
 const app = express();
 const port = process.env.PORT;
-const database = require("./database.js");
 
 app.listen(port, console.log(`server is running on port ${port}`));
 
-app.get("/", async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const users = await database.fetchUsers();
+    const users = await User.find();
     res.send(users);
-  } catch {
-    res.send("error");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/entries", async (req, res) => {
+  try {
+    const entries = await Entry.find();
+    res.send(entries);
+  } catch (error) {
+    res.send(error);
   }
 });
