520c0f39e622c304e7062af22566768d1ecc8647
claracampos <camposclr@gmail.com>
2020-02-19 12:37:30 -0400

    made schema explicit, nested entries in User

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const validator = require("validator");
+const { entrySchema } = require("./entry");
 
-const User = mongoose.model("User", {
+const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
@@ -10,7 +12,10 @@ const User = mongoose.model("User", {
         throw new Error("The e-mail provided is not valid.");
       }
     }
-  }
+  },
+  entries: [entrySchema]
 });
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
