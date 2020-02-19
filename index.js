5ca3f4a396c0cabc955b8fd7c9304563e40c81ba
claracampos <camposclr@gmail.com>
2020-02-19 09:26:31 -0400

    fetched data from database

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT;
+const database = require("./database.js");
 
-app.get("/", (req, res) => {
-  res.send({ testing: "123" });
+app.listen(port, console.log(`server is running on port ${port}`));
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await database.fetchUsers();
+    users
+      .find()
+      .toArray((error, user) => (error ? res.send(error) : res.send(user)));
+  } catch (error) {
+    res.send(error);
+  }
 });
-
-app.listen(3000, console.log("server running"));
