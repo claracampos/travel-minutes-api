6cb998bdb99007d402cdb62dfe03e28964cd945b
claracampos <camposclr@gmail.com>
2020-02-19 11:55:13 -0400

    removed User model (placed in separate file)

--- src/database.js
+++ src/database.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
 
-exports.fetchUsers = async () => {
-  mongoose.connect(process.env.URI, {
+mongoose.connect(
+  process.env.URI,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-  });
-
-  const User = mongoose.model("User", {
-    name: String
-  });
-
-  const users = User.find();
-  return users;
-};
+  },
+  console.log("connected")
+);
