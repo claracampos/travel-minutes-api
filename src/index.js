8dc0dbc452ea5b48b28e47796368d83eca942113
claracampos <camposclr@gmail.com>
2020-02-20 17:11:15 -0400

    changed response

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./database.js");
 const User = require("./models/user");
 const bcrypt = require("bcryptjs");
+const auth = require("./auth");
 
 const app = express();
 const port = process.env.PORT;
@@ -26,8 +27,7 @@ app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const validUser = await User.checkCredentials(email, password);
     const token = await validUser.generateAuthToken();
-    res.status(200);
-    res.send({ validUser, token: token });
+    res.status(200).send({ JWT: token });
   } catch (error) {
     res.status(401);
     res.send({ Error: error.message });
@@ -40,15 +40,14 @@ app.post("/login", async (req, res) => {
 app.post("/register", async (req, res) => {
   try {
     const { email, password } = req.body;
+    await User.doesUserExist(email);
     const newUser = {
       email: email,
       password: await bcrypt.hash(password, 8)
     };
-    await User.doesUserExist(email);
     const savedUser = await User.create(newUser);
     const token = await savedUser.generateAuthToken();
-    res.status(201);
-    res.send({ savedUser, token: token });
+    res.status(201).send({ JWT: token });
   } catch (error) {
     res.status(500);
     res.send({ Error: error.message });
