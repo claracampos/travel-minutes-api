8f986347b45447d972472b6096a727274dcb81c3
claracampos <camposclr@gmail.com>
2020-02-20 14:38:53 -0400

    added schema statics: function doesUserExist

--- src/models/user.js
+++ src/models/user.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const validator = require("validator");
 const { entrySchema } = require("./entry");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    lowercase: true,
+    trim: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error("The e-mail provided is not valid.");
@@ -20,6 +23,13 @@ const UserSchema = new Schema({
   entries: [entrySchema]
 });
 
-const User = mongoose.model("User", UserSchema);
+userSchema.statics.doesUserExist = async function(email) {
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser) {
+    throw new Error("The e-mail provided is already registered.");
+  }
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
