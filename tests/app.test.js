b7090d306182f8ff372eb092d58d9d61f0d6a296
claracampos <camposclr@gmail.com>
2020-03-04 13:47:37 -0400

    replaced testUser with newUser

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,16 +2,9 @@ const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/database/user");
 
-const testUser = {
-  email: "test@gmail.com",
-  password: "testing123",
-  entry: {
-    date: "01/01/2020",
-    place: "test place",
-    seen: "test sights",
-    done: "test actions",
-    met: "test friends"
-  }
+const newUser = {
+  email: "newuser@gmail.com",
+  password: "testing123"
 };
 
 beforeAll(async () => {
@@ -21,34 +14,34 @@ beforeAll(async () => {
 test("Register a new user", async () => {
   await request(app)
     .post("/register")
-    .send({ email: testUser.email, password: testUser.password })
+    .send({ email: newUser.email, password: newUser.password })
     .expect(201);
 });
 
 test("Fail to register an existing user", async () => {
   await request(app)
     .post("/register")
-    .send({ email: testUser.email, password: testUser.password })
+    .send({ email: newUser.email, password: newUser.password })
     .expect(500);
 });
 
 test("Fail to register with an invalid email", async () => {
   await request(app)
     .post("/register")
-    .send({ email: "test", password: testUser.password })
+    .send({ email: "test", password: newUser.password })
     .expect(500);
 });
 
 test("Fail to register with an invalid password (too short)", async () => {
   await request(app)
     .post("/register")
-    .send({ email: testUser.email, password: "123" })
+    .send({ email: newUser.email, password: "123" })
     .expect(500);
 });
 
 test("Fail to register with an invalid password (too long)", async () => {
   await request(app)
     .post("/register")
-    .send({ email: testUser.email, password: "verylongpassword" })
+    .send({ email: newUser.email, password: "verylongpassword" })
     .expect(500);
 });
