0a56662e856a0d3b915a427a66a864bb592bdd60
claracampos <camposclr@gmail.com>
2020-03-04 15:32:54 -0400

    added tests for editing, removing and getting a specific entry

--- tests/entries.test.js
+++ tests/entries.test.js
@@ -1,11 +1,6 @@
 const request = require("supertest");
 const app = require("../src/app");
-const {
-  setUpDatabase,
-  testUserAuth,
-  testEntry,
-  testEntryId
-} = require("./fixture/db.js");
+const { setUpDatabase, testUserAuth, testEntry } = require("./fixture/db.js");
 
 beforeAll(async () => {
   await setUpDatabase();
@@ -32,4 +27,33 @@ test("Add an entry", async () => {
     .set(testUserAuth)
     .send(newEntry)
     .expect(201);
+});
+
+test("Get a single entry", async () => {
+  await request(app)
+    .get(`/entries/${testEntry._id}`)
+    .set(testUserAuth)
+    .expect(200, testEntry);
+});
+
+test("Edit an entry", async () => {
+  await request(app)
+    .patch(`/entries/${testEntry._id}`)
+    .set(testUserAuth)
+    .send({ done: testEntry.done, met: testEntry.met, seen: "updated" })
+    .expect(200, {
+      _id: testEntry._id,
+      date: testEntry.date,
+      place: testEntry.place,
+      seen: "updated",
+      done: testEntry.done,
+      met: testEntry.met
+    });
+});
+
+test("Delete an entry", async () => {
+  await request(app)
+    .delete(`/entries/${testEntry._id}`)
+    .set(testUserAuth)
+    .expect(200);
 });
